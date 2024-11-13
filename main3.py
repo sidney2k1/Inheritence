1e6d3ff5f6fe9fea258db0da0e900e9d3d28e081
sidney2k1 <sidsingla02@gmail.com>
2024-11-13 17:29:00 +0530

    main3

---
+++ main3.py
@@ -0,0 +1,19 @@
+class Bird:
+    def __init__(self):
+        print("Bird is ready")
+    def whoisthis(self):
+        print("Bird")
+    def swim(self):
+        print("Swim faster")
+class penguin(Bird):
+    def __init__(self):
+        super().__init__()
+        print("Penguin is ready")
+    def whoisthis(self):
+        print("Penguin")
+    def run(self):
+        print("Run faster")
+peggy=penguin()
+peggy.whoisthis()
+peggy.swim()
+peggy.run()
